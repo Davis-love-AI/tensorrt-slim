fc2c12f3a1b7a5c947d5640ddb1d9e687584b35f
Paul Balanca <paul.balanca@gmail.com>
2017-07-12 10:08:42 +0100

    UPDATE: ImageNet console.

--- tools/imagenet/imagenet_console.cpp
+++ tools/imagenet/imagenet_console.cpp
@@ -25,15 +25,16 @@
 // FLAGS...
 DEFINE_string(network, "inception1", "ImageNet network to test.");
 DEFINE_string(network_pb, "inception1", "Network protobuf parameter file.");
-DEFINE_string(image, "../data/", "Image to classify.");
 DEFINE_string(imagenet_info, "../data/", "ImageNet information (classes, ...).");
-// DEFINE_bool(verbose, false, "Display program name before message");
+DEFINE_string(image, "../data/", "Image to classify.");
+DEFINE_bool(image_save, false, "Save the result in some new image.");
 // DEFINE_string(message, "Hello world!", "Message to print");
 // DEFINE_int32(input_height, 224, "Network input height.");
 
 /** Global map containing the list of available ImageNet networks.
  */
-std::map<std::string, tfrt::imagenet_network*> NetworksMap = {
+// std::map<std::string, std::unique_ptr<tfrt::imagenet_network> > NetworksMap = {
+std::map<std::string, tfrt::imagenet_network* > NetworksMap = {
     {"inception1", nullptr},
     {"inception2", nullptr}
 };
@@ -42,7 +43,7 @@ std::map<std::string, tfrt::imagenet_network*> NetworksMap = {
 int main( int argc, char** argv )
 {
     bool r;
-    LOG(INFO) << "<<< ImageNet Console | Network " << FLAGS_network << " >>>";
+    LOG(INFO) << "<<< ImageNet-Console | Network " << FLAGS_network << " >>>";
     // Get network and load parameters & weights.
     tfrt::imagenet_network* network = NetworksMap.at(FLAGS_network);
     // network->EnableProfiler();
@@ -50,63 +51,51 @@ int main( int argc, char** argv )
     network->load_info(FLAGS_imagenet_info);
 
     // Load image from file on disk.
+    LOG(INFO) << "Opening image: " << FLAGS_image;
     tfrt::cuda_tensor img("image", {1, 1, 0, 0});
     r = loadImageRGBA(FLAGS_image.c_str(), (float4**)&img.cpu, (float4**)&img.cuda,
         &img.shape.w(), &img.shape.h());
     CHECK(r) << "Fail to read image file: " << FLAGS_image;
 
     // Classifying image.
-    auto rclass = network->classify(img.cuda, img.shape.h(), img.shape.w());
-    // float confidence = 0.0f;
+    auto imgclass = network->classify(img.cuda, img.shape.h(), img.shape.w());
 
-    // // classify image
-    // const int img_class = net->Classify(imgCUDA, imgWidth, imgHeight, &confidence);
+    if(imgclass.first >= 0) {
+        LOG(INFO) << "ImageNet Console classification: "
+            << network->description(imgclass.first) << " with confidence " << imgclass.second;
 
-    // if( img_class >= 0 )
-    // {
-    // 	printf("imagenet-console:  '%s' -> %2.5f%% class #%i (%s)\n", imgFilename, confidence * 100.0f, img_class, net->GetClassDesc(img_class));
+        if(FLAGS_image_save) {
+            std::string  output_filename = FLAGS_image + ".class";
+            // Overlay the classification on the image
+            cudaFont* font = cudaFont::Create();
+            if(font != NULL) {
+                char str[512];
+                sprintf(str, "%2.3f%% %s",
+                    imgclass.second * 100.0f,
+                    network->description(imgclass.first).c_str());
+                const int overlay_x = 10;
+                const int overlay_y = 10;
+                const int px_offset = overlay_y * img.shape.w() * 4 + overlay_x * 4;
 
-    // 	if( argc > 2 )
-    // 	{
-    // 		const char* outputFilename = argv[2];
-
-    // 		// overlay the classification on the image
-    // 		cudaFont* font = cudaFont::Create();
-
-    // 		if( font != NULL )
-    // 		{
-    // 			char str[512];
-    // 			sprintf(str, "%2.3f%% %s", confidence * 100.0f, net->GetClassDesc(img_class));
-
-    // 			const int overlay_x = 10;
-    // 			const int overlay_y = 10;
-    // 			const int px_offset = overlay_y * imgWidth * 4 + overlay_x * 4;
-
-    // 			// if the image has a white background, use black text (otherwise, white)
-    // 			const float white_cutoff = 225.0f;
-    // 			bool white_background = false;
-
-    // 			if( imgCPU[px_offset] > white_cutoff && imgCPU[px_offset + 1] > white_cutoff && imgCPU[px_offset + 2] > white_cutoff )
-    // 				white_background = true;
-
-    // 			// overlay the text on the image
-    // 			font->RenderOverlay((float4*)imgCUDA, (float4*)imgCUDA, imgWidth, imgHeight, (const char*)str, 10, 10,
-    // 							white_background ? make_float4(0.0f, 0.0f, 0.0f, 255.0f) : make_float4(255.0f, 255.0f, 255.0f, 255.0f));
-    // 		}
-
-    // 		printf("imagenet-console:  attempting to save output image to '%s'\n", outputFilename);
-
-    // 		if( !saveImageRGBA(outputFilename, (float4*)imgCPU, imgWidth, imgHeight) )
-    // 			printf("imagenet-console:  failed to save output image to '%s'\n", outputFilename);
-    // 		else
-    // 			printf("imagenet-console:  completed saving '%s'\n", outputFilename);
-    // 	}
-    // }
-    // else
-    // 	printf("imagenet-console:  failed to classify '%s'  (result=%i)\n", imgFilename, img_class);
-
-    // printf("\nshutting down...\n");
-    // CUDA(cudaFreeHost(img.cpu));
-    // delete net;
-    // return 0;
+                // If the image has a white background, use black text (otherwise, white)
+                const float white_cutoff = 225.0f;
+                bool white_background = false;
+                if( img.cpu[px_offset] > white_cutoff && img.cpu[px_offset + 1] > white_cutoff && img.cpu[px_offset + 2] > white_cutoff ) {
+                    white_background = true;
+                }
+                // overlay the text on the image
+                font->RenderOverlay((float4*)img.cuda, (float4*)img.cuda,
+                    img.shape.w(), img.shape.h(), (const char*)str, 10, 10,
+                    white_background ? make_float4(0.0f, 0.0f, 0.0f, 255.0f) : make_float4(255.0f, 255.0f, 255.0f, 255.0f));
+            }
+            LOG(INFO) << "ImageNet Console | Saving the output image to: " << output_filename;
+            r = saveImageRGBA(output_filename.c_str(), (float4*)img.cpu,
+                img.shape.w(), img.shape.h());
+            CHECK(r) << "Failed to save the output image to:" << output_filename;
+        }
+    }
+    else {
+        LOG(WARNING) << "ImageNet Console: failed to classify the image.";
+    }
+    return 0;
 }
