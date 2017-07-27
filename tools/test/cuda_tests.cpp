76e752598169b99b64b0c04f1e7a8152acf971bd
Paul Balanca <paul.balanca@gmail.com>
2017-07-27 17:57:12 +0100

    UPDATE: cuda tests.

--- tools/test/cuda_tests.cpp
+++ tools/test/cuda_tests.cpp
@@ -32,6 +32,12 @@ void cuda_float2half_array(float* host_input, uint16_t* host_output, uint32_t si
 void cuda_half2float_array(uint16_t* host_input, float* host_output, uint32_t size);
 
 
+void print_half(uint16_t* half)
+{
+    uint8_t* ptr = (uint8_t*) half;
+    LOG(INFO) << "HALF: 0x" << std::hex << int(ptr[0]) << int(ptr[1]) << std::endl;
+}
+
 int main(int argc, char **argv)
 {
     google::InitGoogleLogging(argv[0]);
@@ -44,10 +50,10 @@ int main(int argc, char **argv)
     std::vector<float> vec_f2 = {0.0};
     std::vector<uint16_t> vec_h = {0};
 
-    LOG(INFO) << "Original data: " << std::setprecision(10) << vec_f[0] << " | " << vec_f2[0];
+    LOG(INFO) << "Original data: " << std::setprecision(6) << vec_f[0] << " | " << vec_f2[0];
     cuda_float2half_array(vec_f.data(), vec_h.data(), vec_f.size());
     cuda_half2float_array(vec_h.data(), vec_f2.data(), vec_f.size());
-    LOG(INFO) << "Half data: " << std::setprecision(10) << vec_f[0] << " | " << vec_f2[0];
-
+    LOG(INFO) << "Half data: " << std::setprecision(6) << vec_f[0] << " | " << vec_f2[0];
+    print_half(vec_h.data());
     return 0;
 }
