8b5a9a8879b3261ce89e421c5934fc4c475bdf52
Paul Balanca <paul.balanca@gmail.com>
2017-08-12 19:55:53 +0100

    ADD: CUDA CHW image util file.

---
+++ tensorflowrt/cuda/cudaCHWImage.h
@@ -0,0 +1,25 @@
+/* ============================================================================
+# [2017] - Robik AI Ltd - Paul Balanca
+# All Rights Reserved.
+
+# NOTICE: All information contained herein is, and remains
+# the property of Robik AI Ltd, and its suppliers
+# if any.  The intellectual and technical concepts contained
+# herein are proprietary to Robik AI Ltd
+# and its suppliers and may be covered by U.S., European and Foreign Patents,
+# patents in process, and are protected by trade secret or copyright law.
+# Dissemination of this information or reproduction of this material
+# is strictly forbidden unless prior written permission is obtained
+# from Robik AI Ltd.
+# =========================================================================== */
+
+#include "cudaUtility.h"
+
+#ifndef TFRT_CUDA_CHW_IMAGE_H
+#define TFRT_CUDA_CHW_IMAGE_H
+
+/** Convert a HWC image to CHW format.
+ */
+ cudaError_t cuda_hwc_to_chw(float* h_input, float* d_output, uint32_t height, uint32_t width);
+
+#endif
