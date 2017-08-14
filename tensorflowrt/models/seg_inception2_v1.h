baf6c33af74ed2d29864d7e204927c22c5ac2182
Paul Balanca <paul.balanca@gmail.com>
2017-08-14 19:21:12 +0100

    UPDATE: seg inception2 model.

--- tensorflowrt/models/seg_inception2_v1.h
+++ tensorflowrt/models/seg_inception2_v1.h
@@ -123,7 +123,7 @@ public:
             net = seg_inception2_extra_feature(net, net_in, ssc.sub(feat_names[i]), feat_size[i]);
         }
         // Last convolution layer and softmax.
-        net = seg_inception2_last_layer(net, ssc.sub("block10"), 18);
+        net = seg_inception2_last_layer(net, ssc.sub("block10"), this->num_classes());
         net = tfrt::softmax(sc, "Softmax")(net);
 
         // Clear any cached stuff...
