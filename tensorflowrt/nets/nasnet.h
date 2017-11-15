873fd2f027a4bd14675c27b83bd14d1dabc40217
Paul Balanca <paul.balanca@gmail.com>
2017-11-15 11:56:31 +0000

    UPDATE: nasnet general structure, defined.

--- tensorflowrt/nets/nasnet.h
+++ tensorflowrt/nets/nasnet.h
@@ -26,6 +26,8 @@ namespace nasnet
  */
 typedef tfrt::separable_convolution2d<tfrt::ActivationType::NONE, tfrt::PaddingType::SAME, true> separable_conv2d;
 typedef tfrt::convolution2d<tfrt::ActivationType::NONE, tfrt::PaddingType::SAME, true>  conv2d;
+typedef tfrt::convolution2d<tfrt::ActivationType::NONE, tfrt::PaddingType::VALID, true>  conv2d_valid;
+        
 typedef tfrt::max_pooling2d<tfrt::PaddingType::SAME>    max_pool2d;
 typedef tfrt::avg_pooling2d<tfrt::PaddingType::SAME>    avg_pool2d;
 typedef tfrt::concat_channels                           concat_channels;
@@ -40,8 +42,6 @@ public:
         m_scope{sc}, m_num_conv_filters{num_filters}, m_filter_scaling{filter_scaling}
     {
     }
-    
-public:
     /** Base of every cell: 1x1 convolution. */
     nvinfer1::ITensor* base_cell_n(nvinfer1::ITensor* net, tfrt::scope sc) const
     {
@@ -69,7 +69,7 @@ public:
             net = avg_pool2d(sc).ksize(3).stride(2)(net);
         }
         // Number of channels different?
-        if (fsize != shape_n_1.c()) {
+        if (int(fsize) != shape_n_1.c()) {
             net = conv2d(sc, "1x1").noutputs(fsize).ksize(1)(net);
         }
         return net;
@@ -79,7 +79,7 @@ public:
         size_t stride, size_t num_outputs, bool relu=false) const
     {
         auto shape = tfrt::dims_nchw(net);
-        if (stride > 1 || shape.c() != num_outputs) {
+        if (stride > 1 || shape.c() != int(num_outputs)) {
             if (relu) {
                 net = tfrt::relu(sc)(net);
             }
@@ -102,7 +102,6 @@ public:
         }
         return net;
     }
-
 public:
     size_t filter_size() const {
         return size_t(m_num_conv_filters * m_filter_scaling);
@@ -254,10 +253,47 @@ public:
             m_num_conv_filters{num_conv_filters}
     {
     }
-
+    /** Build the network. Whohohoooo! */
     virtual nvinfer1::ITensor* build(tfrt::scope sc) {
         auto net = tfrt::input(sc)();
-        //
+        
+        // ImageNet stem cells.
+        auto cell_outputs = imagenet_stem(net, sc);
+
+        // Core of the network.
+        auto reduction_cells = this->reduction_layers();
+        double filter_scaling = 1.0;
+        size_t true_cell_num = 2;
+        for (size_t i = 0 ; i < m_num_cells ; ++i) {
+            // Reduction cell?
+            if (reduction_cells[i]) {
+                filter_scaling *= m_filter_scaling_rate;
+                // Scope name + cell construction...
+                std::ostringstream ostr("reduction_cell_");   ostr << i;
+                auto ssc = sc.sub(ostr.str());
+                auto cell = reduction_cell(ssc, m_num_conv_filters, filter_scaling);
+                // Apply!
+                int n = cell_outputs.size();
+                net = cell(cell_outputs[n-1], cell_outputs[n-2]);
+                cell_outputs.push_back(net);
+                true_cell_num++;
+            }
+
+            // Scope name + normal cell construction...
+            std::ostringstream ostr("cell_");   ostr << i;
+            auto ssc = sc.sub(ostr.str());
+            auto cell = normal_cell(ssc, m_num_conv_filters, filter_scaling);
+            // Apply!
+            int n = cell_outputs.size();
+            net = cell(cell_outputs[n-1], cell_outputs[n-2]);
+            cell_outputs.push_back(net);
+            true_cell_num++;
+        }
+        // Logits?
+        // auto ssc = sc.sub("Logits");
+        // net = avg_pool2d(ssc, "AvgPool_1a_7x7").ksize({7, 7})(net);
+        // net = conv2d(ssc, "Conv2d_1c_1x1").noutputs(num_classes).ksize({1, 1})(net);
+        net = tfrt::softmax(sc, "Softmax")(net);
         return net;
     }
 
@@ -267,7 +303,7 @@ protected:
         // First 3x3 convolution.
         size_t num_stem_filters = int(32 * m_stem_multiplier);
         // VALID padding?
-        net = conv2d(sc, "conv0").ksize(3).stride(2)(net);
+        net = conv2d_valid(sc, "conv0").ksize(3).stride(2).noutputs(num_stem_filters)(net);
 
         // Reduction cells.
         size_t num_stem_cells = 2;
@@ -276,12 +312,13 @@ protected:
         cell_outputs.push_back(net);
 
         // Filter scaling?
-        float filter_scaling = 1.0f / pow(m_filter_scaling_rate, float(num_stem_cells));        
+        double filter_scaling = 1.0 / pow(double(m_filter_scaling_rate), double(num_stem_cells));
         for (size_t i = 0 ; i < num_stem_cells ; ++i) {
             // Scope name + cell construction...
             std::ostringstream ostr("cell_stem_");   ostr << i;
             auto ssc = sc.sub(ostr.str());
             auto cell = reduction_cell(ssc, m_num_conv_filters, filter_scaling);
+
             // Apply!
             int n = cell_outputs.size();
             net = cell(cell_outputs[n-1], cell_outputs[n-2]);
@@ -314,203 +351,57 @@ protected:
     size_t  m_num_conv_filters;
 };
 
-//   stem_multiplier=1.0,
-//       dense_dropout_keep_prob=0.5,
-//       num_cells=12,
-//       filter_scaling_rate=2.0,
-//       drop_path_keep_prob=1.0,
-//       num_conv_filters=44,
-//       use_aux_head=1,
-//       num_reduction_layers=2,
-//       data_format='NHWC',
-//       skip_reduction_layer_input=0,
-//       total_training_steps=250000,
 
-// /** Major mixed block used in Inception v2 (4 branches).
-//  * Average pooling version.
-//  */
-// template <int B0, int B10, int B11, int B20, int B21, int B3>
-// inline nvinfer1::ITensor* block_mixed_avg(nvinfer1::ITensor* input, tfrt::scope sc,
-//                                           tfrt::map_tensor* end_points=nullptr)
-// {
-//     nvinfer1::ITensor* net{input};
-//     // Branch 0.
-//     auto ssc = sc.sub("Branch_0");
-//     auto branch0 = conv2d(ssc, "Conv2d_0a_1x1").noutputs(B0).ksize({1, 1})(net);
-//     // Branch 1.
-//     ssc = sc.sub("Branch_1");
-//     auto branch1 = conv2d(ssc, "Conv2d_0a_1x1").noutputs(B10).ksize({1, 1})(net);
-//     branch1 = conv2d(ssc, "Conv2d_0b_3x3").noutputs(B11).ksize({3, 3})(branch1);
-//     // Branch 2.
-//     ssc = sc.sub("Branch_2");
-//     auto branch2 = conv2d(ssc, "Conv2d_0a_1x1").noutputs(B20).ksize({1, 1})(net);
-//     branch2 = conv2d(ssc, "Conv2d_0b_3x3").noutputs(B21).ksize({3, 3})(branch2);
-//     branch2 = conv2d(ssc, "Conv2d_0c_3x3").noutputs(B21).ksize({3, 3})(branch2);
-//     // Branch 2.
-//     ssc = sc.sub("Branch_3");
-//     auto branch3 = avg_pool2d(ssc, "AvgPool_0a_3x3").ksize({3, 3})(net);
-//     branch3 = conv2d(ssc, "Conv2d_0b_1x1").noutputs(B3).ksize({1, 1})(branch3);
-//     // Concat everything!
-//     net = concat_channels(sc)({branch0, branch1, branch2, branch3});
-//     return tfrt::add_end_point(end_points, sc.name(), net);
-// }
-// /** Major mixed block used in Inception v2 (4 branches).
-//  * Max pooling version.
-//  */
-// template <int B0, int B10, int B11, int B20, int B21, int B3>
-// inline nvinfer1::ITensor* block_mixed_max(nvinfer1::ITensor* input, tfrt::scope sc,
-//                                           tfrt::map_tensor* end_points=nullptr)
-// {
-//     nvinfer1::ITensor* net{input};
-//     // Branch 0.
-//     auto ssc = sc.sub("Branch_0");
-//     auto branch0 = conv2d(ssc, "Conv2d_0a_1x1").noutputs(B0).ksize({1, 1})(net);
-//     // Branch 1.
-//     ssc = sc.sub("Branch_1");
-//     auto branch1 = conv2d(ssc, "Conv2d_0a_1x1").noutputs(B10).ksize({1, 1})(net);
-//     branch1 = conv2d(ssc, "Conv2d_0b_3x3").noutputs(B11).ksize({3, 3})(branch1);
-//     // Branch 2.
-//     ssc = sc.sub("Branch_2");
-//     auto branch2 = conv2d(ssc, "Conv2d_0a_1x1").noutputs(B20).ksize({1, 1})(net);
-//     branch2 = conv2d(ssc, "Conv2d_0b_3x3").noutputs(B21).ksize({3, 3})(branch2);
-//     branch2 = conv2d(ssc, "Conv2d_0c_3x3").noutputs(B21).ksize({3, 3})(branch2);
-//     // Branch 2.
-//     ssc = sc.sub("Branch_3");
-//     auto branch3 = max_pool2d(ssc, "MaxPool_0a_3x3").ksize({3, 3})(net);
-//     branch3 = conv2d(ssc, "Conv2d_0b_1x1").noutputs(B3).ksize({1, 1})(branch3);
-//     // Concat everything!
-//     net = concat_channels(sc)({branch0, branch1, branch2, branch3});
-//     return tfrt::add_end_point(end_points, sc.name(), net);
-// }
-// /** Specific mixed block with stride 2 used in Inception v2.
-//  */
-// template <int B00, int B01, int B10, int B11>
-// inline nvinfer1::ITensor* block_mixed_s2(nvinfer1::ITensor* input, tfrt::scope sc,
-//                                           tfrt::map_tensor* end_points=nullptr)
-// {
-//     nvinfer1::ITensor* net{input};
-//     // Branch 0.
-//     auto ssc = sc.sub("Branch_0");
-//     auto branch0 = conv2d(ssc, "Conv2d_0a_1x1").noutputs(B00).ksize({1, 1})(net);
-//     branch0 = conv2d(ssc, "Conv2d_1a_3x3").noutputs(B01).ksize({3, 3}).stride({2, 2})(branch0);
-//     // Branch 2.
-//     ssc = sc.sub("Branch_1");
-//     auto branch1 = conv2d(ssc, "Conv2d_0a_1x1").noutputs(B10).ksize({1, 1})(net);
-//     branch1 = conv2d(ssc, "Conv2d_0b_3x3").noutputs(B11).ksize({3, 3})(branch1);
-//     branch1 = conv2d(ssc, "Conv2d_1a_3x3").noutputs(B11).ksize({3, 3}).stride({2, 2})(branch1);
-//     // Branch 2.
-//     ssc = sc.sub("Branch_2");
-//     auto branch2 = max_pool2d(ssc, "MaxPool_1a_3x3").ksize({3, 3}).stride({2, 2})(net);
-//     // Concat everything!
-//     net = concat_channels(sc)({branch0, branch1, branch2});
-//     return tfrt::add_end_point(end_points, sc.name(), net);
-// }
-
-// /* ============================================================================
-//  * Inception2 blocks 1 to 5.
-//  * ========================================================================== */
-// inline nvinfer1::ITensor* block1(nvinfer1::ITensor* net, tfrt::scope sc,
-//                                  tfrt::map_tensor* end_points=nullptr)
-// {
-//     int depthwise_multiplier = std::min(int(64 / 3), 8);
-//     // // 7x7 depthwise convolution.
-//     net = separable_conv2d(sc, "Conv2d_1a_7x7")
-//         .depthmul(depthwise_multiplier)
-//         .noutputs(64).ksize({7, 7}).stride({2, 2})(net);
-//     // net = max_pool2d(sc, "MaxPool_1a_3x3").ksize({3, 3}).stride({2, 2})(net);
-//     return net;
-// }
-// inline nvinfer1::ITensor* block2(nvinfer1::ITensor* net, tfrt::scope sc,
-//                                  tfrt::map_tensor* end_points=nullptr)
-// {
-//     net = max_pool2d(sc, "MaxPool_2a_3x3").ksize({3, 3}).stride({2, 2})(net);
-//     net = conv2d(sc, "Conv2d_2b_1x1").noutputs(64).ksize({1, 1})(net);
-//     net = conv2d(sc, "Conv2d_2c_3x3").noutputs(192).ksize({3, 3})(net);
-//     return tfrt::add_end_point(end_points, sc.sub("Conv2d_2c_3x3").name(), net);;
-// }
-// inline nvinfer1::ITensor* block3(nvinfer1::ITensor* net, tfrt::scope sc,
-//                                  tfrt::map_tensor* end_points=nullptr)
-// {
-//     // Mixed block 3b and 3c.
-//     net = max_pool2d(sc, "MaxPool_3a_3x3").ksize({3, 3}).stride({2, 2})(net);
-//     net = block_mixed_avg<64, 64, 64, 64, 96, 32>(net, sc.sub("Mixed_3b"), end_points);
-//     net = block_mixed_avg<64, 64, 96, 64, 96, 64>(net, sc.sub("Mixed_3c"), end_points);
-//     return net;
-// }
-// inline nvinfer1::ITensor* block4(nvinfer1::ITensor* net, tfrt::scope sc,
-//                                  tfrt::map_tensor* end_points=nullptr)
-// {
-//     // Mixed blocks 4a to 4e.
-//     net = block_mixed_s2<128, 160, 64, 96>(net, sc.sub("Mixed_4a"));
-//     net = block_mixed_avg<224, 64, 96, 96, 128, 128>(net, sc.sub("Mixed_4b"), end_points);
-//     net = block_mixed_avg<192, 96, 128, 96, 128, 128>(net, sc.sub("Mixed_4c"), end_points);
-//     net = block_mixed_avg<160, 128, 160, 128, 160, 96>(net, sc.sub("Mixed_4d"), end_points);
-//     net = block_mixed_avg<96, 128, 192, 160, 192, 96>(net, sc.sub("Mixed_4e"), end_points);
-//     return net;
-// }
-// inline nvinfer1::ITensor* block5(nvinfer1::ITensor* net, tfrt::scope sc,
-//                                  tfrt::map_tensor* end_points=nullptr)
-// {
-//     // Mixed blocks 5a to 5c.
-//     net = block_mixed_s2<128, 192, 192, 256>(net, sc.sub("Mixed_5a"));
-//     net = block_mixed_avg<352, 192, 320, 160, 224, 128>(net, sc.sub("Mixed_5b"), end_points);
-//     net = block_mixed_max<352, 192, 320, 192, 224, 128>(net, sc.sub("Mixed_5c"), end_points);
-//     return net;
-// }
-
-// /* ============================================================================
-//  * Inception2 network: functional declaration.
-//  * ========================================================================== */
-// inline nvinfer1::ITensor* base(nvinfer1::ITensor* input, tfrt::scope sc,
-//                                tfrt::map_tensor* end_points=nullptr)
-// {
-//     nvinfer1::ITensor* net{input};
-//     // Main blocks 1 to 5.
-//     net = block1(net, sc, end_points);
-//     net = block2(net, sc, end_points);
-//     net = block3(net, sc, end_points);
-//     net = block4(net, sc, end_points);
-//     net = block5(net, sc, end_points);
-//     return net;
-// }
-// inline nvinfer1::ITensor* inception2(nvinfer1::ITensor* input,
-//                                      tfrt::scope sc,
-//                                      int num_classes=1001)
-// {
-//     nvinfer1::ITensor* net;
-//     // Construct backbone network.
-//     net = base(input, sc);
-//     // Logits end block.
-//     {
-//         typedef tfrt::avg_pooling2d<tfrt::PaddingType::VALID>  avg_pool2d;
-//         typedef tfrt::convolution2d<tfrt::ActivationType::NONE, tfrt::PaddingType::SAME, false>  conv2d;
-//         auto ssc = sc.sub("Logits");
-//         net = avg_pool2d(ssc, "AvgPool_1a_7x7").ksize({7, 7})(net);
-//         net = conv2d(ssc, "Conv2d_1c_1x1").noutputs(num_classes).ksize({1, 1})(net);
-//     }
-//     net = tfrt::softmax(sc, "Softmax")(net);
-//     return net;
-// }
-
-// /* ============================================================================
-//  * Inception2 class: as imagenet network.
-//  * ========================================================================== */
-// class net : public tfrt::imagenet_network
-// {
-// public:
-//     /** Constructor with default name.  */
-//     net() : tfrt::imagenet_network("InceptionV2", 1000, true) {}
-
-//     /** Inception2 building method. Take a network scope and do the work!
-//      */
-//     virtual nvinfer1::ITensor* build(tfrt::scope sc) {
-//         auto net = tfrt::input(sc)();
-//         // auto net = tfrt::input(sc).shape({64, 112, 112})();
-//         net = inception2(net, sc, 1001);
-//         return net;
-//     }
-// };
 
 }
 
+/* ============================================================================
+ * NASNet mobile.
+ * ========================================================================== */
+namespace nasnet_mobile
+{
+class net : public nasnet::net
+{
+public:
+    /** NASNet mobile definition. */
+    net() : nasnet::net("nasnet_mobile", 1.0, 2.0, 12, 2, 44)
+    {}
+
+    // stem_multiplier=1.0,
+    // dense_dropout_keep_prob=0.5,
+    // num_cells=12,
+    // filter_scaling_rate=2.0,
+    // drop_path_keep_prob=1.0,
+    // num_conv_filters=44,
+    // use_aux_head=1,
+    // num_reduction_layers=2,
+    // data_format='NHWC',
+    // skip_reduction_layer_input=0,
+    // total_training_steps=250000,
+};
+}
+/* ============================================================================
+ * NASNet large.
+ * ========================================================================== */
+namespace nasnet_large
+{
+class net : public nasnet::net
+{
+public:
+    /** NASNet mobile definition. */
+    net() : nasnet::net("nasnet_large", 3.0, 2.0, 18, 2, 168)
+    {}
+    // stem_multiplier=3.0,
+    // dense_dropout_keep_prob=0.5,
+    // num_cells=18,
+    // filter_scaling_rate=2.0,
+    // num_conv_filters=168,
+    // drop_path_keep_prob=drop_path_keep_prob,
+    // use_aux_head=1,
+    // num_reduction_layers=2,
+    // data_format='NHWC',
+    // skip_reduction_layer_input=1,
+    // total_training_steps=250000,
+};
+}
 #endif
