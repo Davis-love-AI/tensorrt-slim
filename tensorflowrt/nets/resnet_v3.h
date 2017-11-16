d1eeee6f661f285f5120a3b4ee08b8118b39885e
Paul Balanca <paul.balanca@gmail.com>
2017-11-16 17:35:00 +0000

    UPDATE: resnet v3, num of groups.

--- tensorflowrt/nets/resnet_v3.h
+++ tensorflowrt/nets/resnet_v3.h
@@ -76,7 +76,7 @@ inline nvinfer1::ITensor* bottleneck(nvinfer1::ITensor* input, int outdepth, int
     // Shortcut.
     net = shortcut(input, outdepth, stride, ssc);
     // Residual part.
-    res = conv2d_grouped(ssc, "conv1").ngroups(2).noutputs(bndepth).ksize(1).stride(stride)(input);
+    res = conv2d_grouped(ssc, "conv1").ngroups(4).noutputs(bndepth).ksize(1).stride(stride)(input);
     // res = conv2d_none(ssc, "conv1").noutputs(bndepth).ksize(1).stride(stride)(input);
 
     // Grouped 3x3 convolution.
@@ -89,7 +89,7 @@ inline nvinfer1::ITensor* bottleneck(nvinfer1::ITensor* input, int outdepth, int
     // res = concat_channels(sc)({res1, res2});
 
     // res = conv2d(ssc, "conv3").noutputs(outdepth).ksize(1).stride(1)(res);
-    res = conv2d_grouped(ssc, "conv3").ngroups(2).noutputs(outdepth).ksize(1).stride(1)(res);
+    res = conv2d_grouped(ssc, "conv3").ngroups(4).noutputs(outdepth).ksize(1).stride(1)(res);
 
     // Add the final result!
     net = tfrt::add(ssc, "add")(net, res);
