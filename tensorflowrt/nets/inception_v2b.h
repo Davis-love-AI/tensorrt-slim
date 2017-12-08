f5faffe2d1be7f7366c1f9442130475d80add4c9
Paul Balanca <paul.balanca@gmail.com>
2017-12-08 15:54:03 +0000

    UPDATE: inception v2b

--- tensorflowrt/nets/inception_v2b.h
+++ tensorflowrt/nets/inception_v2b.h
@@ -262,10 +262,11 @@ inline nvinfer1::ITensor* block1(nvinfer1::ITensor* net, tfrt::scope sc,
 {
     int depthwise_multiplier = std::min(int(64 / 3), 8);
     // // 7x7 depthwise convolution.
-    net = separable_conv2d(sc, "Conv2d_1a_7x7")
-        .dw_group_size(1024)
-        .depthmul(depthwise_multiplier)
-        .noutputs(64).ksize({7, 7}).stride({2, 2})(net);
+    // net = separable_conv2d(sc, "Conv2d_1a_7x7")
+    //     .dw_group_size(1024)
+    //     .depthmul(depthwise_multiplier)
+    //     .noutputs(64).ksize({7, 7}).stride({2, 2})(net);
+    net = conv2d(sc, "Conv2d_1a_7x7").noutputs(64).ksize(7).stride(2)(net);
     // net = max_pool2d(sc, "MaxPool_1a_3x3").ksize({3, 3}).stride({2, 2})(net);
     return net;
 }
@@ -276,16 +277,16 @@ inline tensor_pair block2(nvinfer1::ITensor* net, tfrt::scope sc,
     net = max_pool2d(sc, "MaxPool_2a_3x3").ksize({3, 3}).stride({2, 2})(net);
     net = conv2d(sc, "Conv2d_2b_1x1").noutputs(64).ksize({1, 1})(net);
 
-    net = dw_conv2d(sc, "Conv2d_2c_3x3_dw").depthmul(2).ksize(3)(net);
-    outputs.first = conv2d(sc, "Conv2d_2c_1x1l").noutputs(192/2).ksize(1)(net);
-    outputs.second = conv2d(sc, "Conv2d_2c_1x1r").noutputs(192/2).ksize(1)(net);
+    // net = dw_conv2d(sc, "Conv2d_2c_3x3_dw").depthmul(2).ksize(3)(net);
+    // outputs.first = conv2d(sc, "Conv2d_2c_1x1l").noutputs(192/2).ksize(1)(net);
+    // outputs.second = conv2d(sc, "Conv2d_2c_1x1r").noutputs(192/2).ksize(1)(net);
 
-    // outputs.first = separable_conv2d(sc, "Conv2d_2c_3x3l")
-    //     .dw_group_size(1).depthmul(1)
-    //     .noutputs(192/2).ksize(3)(net);
-    // outputs.second = separable_conv2d(sc, "Conv2d_2c_3x3r")
-    //     .dw_group_size(1).depthmul(1)
-    //     .noutputs(192/2).ksize(3)(net);
+    outputs.first = separable_conv2d(sc, "Conv2d_2c_3x3l")
+        .dw_group_size(1).depthmul(1)
+        .noutputs(192/2).ksize(3)(net);
+    outputs.second = separable_conv2d(sc, "Conv2d_2c_3x3r")
+        .dw_group_size(1).depthmul(1)
+        .noutputs(192/2).ksize(3)(net);
     // outputs.first = conv2d(sc, "Conv2d_2c_3x3l").noutputs(192/2).ksize({3, 3})(net);
     // outputs.second = conv2d(sc, "Conv2d_2c_3x3r").noutputs(192/2).ksize({3, 3})(net);
     return outputs;
