33f5b9b8f475702f0d0ba56e09c5cc56aecad9ff
Paul Balanca <paul.balanca@gmail.com>
2018-01-02 17:49:32 +0000

    FIX: inception v2c shuffling.

--- tensorflowrt/nets/inception_v2c.h
+++ tensorflowrt/nets/inception_v2c.h
@@ -70,24 +70,25 @@ template <int B0, int B10, int B11, int B20, int B21, int B3>
 inline nvinfer1::ITensor* block_mixed_avg(nvinfer1::ITensor* input, tfrt::scope sc,
                                           tfrt::map_tensor* end_points=nullptr)
 {
-    int factor = 4;
+    int factor = 2;
+    int alpha = factor * factor;
 
     nvinfer1::ITensor* net{input};
     net = channel_to_hw(net, sc, factor, factor);
     // Branch 0.
     auto ssc = sc.sub("Branch_0");
-    auto branch0 = conv2d(ssc, "Conv2d_0a_1x1").noutputs(B0).ksize({1, 1})(net);
+    auto branch0 = conv2d(ssc, "Conv2d_0a_1x1").noutputs(B0 / alpha).ksize({1, 1})(net);
     branch0 = hw_to_channel(branch0, ssc, factor, factor);
     // Branch 1.
     ssc = sc.sub("Branch_1");
-    auto branch1 = conv2d(ssc, "Conv2d_0a_1x1").noutputs(B10).ksize({1, 1})(net);
+    auto branch1 = conv2d(ssc, "Conv2d_0a_1x1").noutputs(B10 / alpha).ksize({1, 1})(net);
     branch1 = hw_to_channel(branch1, ssc, factor, factor);
     // branch1 = conv2d(ssc, "Conv2d_0b_3x3").noutputs(B11).ksize({3, 3})(branch1);
     branch1 = dw_conv2d(ssc, "Conv2d_0b_3x3").ksize({3, 3}).noutputs(B10)(branch1);
 
     // Branch 2.
     ssc = sc.sub("Branch_2");
-    auto branch2 = conv2d(ssc, "Conv2d_0a_1x1").noutputs(B20).ksize({1, 1})(net);
+    auto branch2 = conv2d(ssc, "Conv2d_0a_1x1").noutputs(B20 / alpha).ksize({1, 1})(net);
     branch2 = hw_to_channel(branch2, ssc, factor, factor);
     // branch2 = conv2d(ssc, "Conv2d_0b_3x3").noutputs(B21).ksize({3, 3})(branch2);
     // branch2 = conv2d(ssc, "Conv2d_0c_3x3").noutputs(B21).ksize({3, 3})(branch2);
@@ -97,7 +98,7 @@ inline nvinfer1::ITensor* block_mixed_avg(nvinfer1::ITensor* input, tfrt::scope 
 
     // Branch 2.
     ssc = sc.sub("Branch_3");
-    auto branch3 = conv2d(ssc, "Conv2d_0b_1x1").noutputs(B3).ksize({1, 1})(net);
+    auto branch3 = conv2d(ssc, "Conv2d_0b_1x1").noutputs(B3 / alpha).ksize({1, 1})(net);
     branch3 = hw_to_channel(branch3, ssc, factor, factor);
     branch3 = avg_pool2d(ssc, "AvgPool_0a_3x3").ksize({3, 3})(branch3);
 
